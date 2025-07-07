abdfcda924961ffe74c646546402112f925fc421
jacquikeating <j.keating@live.ca>
2025-07-07 16:35:27 -0400

    Styled Entry
    - Applied CSS styles to Entry component according to Figma design.
    - Added a :hover and :focus state for links.
    - Removed unnecessary main-image-container element.
    - Added or renamed classNames for better clarity.

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,13 +1,11 @@
 export default function Entry() {
   return (
     <article className="journal-entry">
-      <div className="main-image-container">
-        <img
-          className="main-image"
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="Mount Fuji"
-        />
-      </div>
+      <img
+        className="entry-image"
+        src="https://scrimba.com/links/travel-journal-japan-image-url"
+        alt="Mount Fuji"
+      />
       <div className="info-container">
         <img
           className="marker"
@@ -15,7 +13,10 @@ export default function Entry() {
           alt="map marker icon"
         />
         <span className="country">Japan</span>
-        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+        <a
+          href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+          className="maps-link"
+        >
           View on Google Maps
         </a>
         <h2 className="entry-title">Mount Fuji</h2>
