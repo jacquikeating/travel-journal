788a7c80100438944f9e6890073e5372fc1f60e8
jacquikeating <j.keating@live.ca>
2025-07-07 16:41:30 -0400

    Refactored Entry to use props
    - Refactored Entry component, replacing all hardcoded data with props.
    - Passed props for Mount Fuji to first Entry component in App.jsx.

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,31 +1,20 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="journal-entry">
-      <img
-        className="entry-image"
-        src="https://scrimba.com/links/travel-journal-japan-image-url"
-        alt="Mount Fuji"
-      />
+      <img className="entry-image" src={props.img.src} alt="Mount Fuji" />
       <div className="info-container">
         <img
           className="marker"
-          src="src/assets/marker.png"
-          alt="map marker icon"
+          src="/src/assets/marker.png"
+          alt={props.img.alt}
         />
-        <span className="country">Japan</span>
-        <a
-          href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-          className="maps-link"
-        >
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapsLink} className="maps-link">
           View on Google Maps
         </a>
-        <h2 className="entry-title">Mount Fuji</h2>
-        <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="entry-text">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );
